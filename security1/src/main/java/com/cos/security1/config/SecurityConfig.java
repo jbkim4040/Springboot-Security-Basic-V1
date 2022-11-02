c17ef4ad6850a5a7e0a4aa24865c65fffa9ce9fb
gimjeongbin <jbkim404037@gmail.com>
2022-11-03 00:08:28 +0900

    outh2 구글로그인 준비

--- security1/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -30,6 +30,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.formLogin()
 			.loginPage("/loginForm")
 			.loginProcessingUrl("/login") // login 주소가 호출이 되면 시큐리티가 낚아채서 security가 대신 로그인을 진행해줌 
-			.defaultSuccessUrl("/");
+			.defaultSuccessUrl("/")
+			.and()
+			.oauth2Login()
+			.loginPage("/loginForm") // 구글 로그인이 완료된 후처리가 필요함
+			;
 	}
 }
